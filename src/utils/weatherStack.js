ade1f15e2300abc925fd2d4d3602ec5e5deecfb8
Ashok Prajapati <ashokpra9940@gmail.com>
2020-05-05 12:25:53 +0545

    add humidity in weather desc

--- src/utils/weatherStack.js
+++ src/utils/weatherStack.js
@@ -1,6 +1,7 @@
 const postmanRequest = require('postman-request');
 
 const weatherStack = (longitude, latitude, callback) => {
+    console.log(longitude, latitude)
     const url = 'http://api.weatherstack.com/current?access_key=b017f6d6c52f8e1bc79dc8b578b1ef2b&query='+longitude+','+latitude+'';
     postmanRequest({url, json: true}, (error, { body }) => {
         if(error) {
@@ -8,7 +9,8 @@ const weatherStack = (longitude, latitude, callback) => {
         } else if(body.error) {
             callback('Unable to find the Location.');
         } else {
-            callback(undefined, body.current.weather_descriptions[0]+ " Currently the temperature is " + body.current.temperature+ " degree celsius. It is feels like " + body.current.feelslike+" degree celcius." );
+            callback(undefined, 
+            body.current.weather_descriptions[0]+ " Currently the temperature is " + body.current.temperature+ " degree celsius. It is feels like " + body.current.feelslike+" degree celcius. The current Humidity is " + body.current.humidity+ " degree."  );
         }
     })
 }
